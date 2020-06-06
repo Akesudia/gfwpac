6801dff903eb624ba9d542f26b0f998a2083ced5
Akesudia <akesudia@163.com>
2020-06-06 19:43:12 +0800

    [2020年 06月 06日 星期六 19:43:12 CST] update

--- generate.sh
+++ generate.sh
@@ -6,10 +6,17 @@ rm -f ./gfwlist.pac
 #use jsdelivr CDN
 wget https://cdn.jsdelivr.net/gh/petronny/gfwlist2pac/gfwlist.pac
 sed -i 's|127.0.0.1:1080|127.0.0.1:1088|g' gfwlist.pac
+
 #simple add needed domain
 sed -i '/"google.com",/a\            "drweb.com",' gfwlist.pac
 sed -i '/"google.com",/a\            "kaspersky-labs.com",' gfwlist.pac
 sed -i '/"google.com",/a\            "asciinema.org",' gfwlist.pac
+sed -i '/"google.com",/a\            "sourceforge.io",' gfwlist.pac
+sed -i '/"google.com",/a\            "sourceforge.net",' gfwlist.pac
+sed -i '/"google.com",/a\            "gamepedia.com",' gfwlist.pac
+
+
+
 
 git add .
 git commit -m "[$(LANG=C date)] update"
