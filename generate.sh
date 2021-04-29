c32103a97edde671681bdc603a4f53b41456cb64
Akesudia <akesudia@163.com>
2021-04-29 09:58:41 +0800

    [2021年 04月 29日 星期四 09:58:41 CST] update

--- generate.sh
+++ generate.sh
@@ -6,6 +6,7 @@ rm -f ./gfwlist.pac
 #use jsdelivr CDN
 wget https://cdn.jsdelivr.net/gh/petronny/gfwlist2pac/gfwlist.pac
 sed -i 's|127.0.0.1:1080|127.0.0.1:1088|g' gfwlist.pac
+#sed -i 's|127.0.0.1:1080|127.0.0.1:1088|g' gfwlist.pac
 
 #simple add needed domain
 sed -i '/"google.com",/a\            "drweb.com",' gfwlist.pac
